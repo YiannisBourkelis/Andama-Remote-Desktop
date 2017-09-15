0cb56f36756c1965a1164d914242900caf7262c9
Yiannis Bourkelis <eu_gr_yiannis@hotmail.com>
2017-09-15 11:53:24 +0300

    For some reason QtInfoMsg is not declared in Qt 5.7.1 for Mac OS

--- src/Andama/main.cpp
+++ src/Andama/main.cpp
@@ -46,11 +46,13 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
     case QtDebugMsg:
         txt = QString("Debug: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
         fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-        break;
+        break;  
+        //TODO: For some reason QtInfoMsg is not declared in Qt 5.7.1 for Mac OS
+#ifndef Q_OS_MAC
     case QtInfoMsg:
         txt = QString("Info: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
         fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-        break;
+#endif
     case QtWarningMsg:
         txt = QString("Warning: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
         fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
