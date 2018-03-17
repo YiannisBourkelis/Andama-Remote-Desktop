a3e87b8f5ee9c6c85d8cf66cee9269efcf9c8895
Daniele <daniele.delorenzi@fastnetserv.net>
2018-03-17 20:57:00 +0100

    Added italian translation

---
+++ src/Andama/Andama_it.ts
@@ -0,0 +1,320 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it" sourcelanguage="en">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="About.ui" line="32"/>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="About.ui" line="54"/>
+        <location filename="About.ui" line="73"/>
+        <source>TextLabel</source>
+        <translation>Etichetta di testo</translation>
+    </message>
+    <message>
+        <location filename="About.ui" line="92"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Copyright &amp;copy; 2017 Yiannis Bourkelis&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Copyright &amp;copy; 2018 Yiannis Bourkelis&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="About.ui" line="121"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Arial&apos;; font-size:12pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;LICENSE&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;.Lucida Grande UI&apos;;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;Andama is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. &lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;Andama is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Arial&apos;; font-size:12pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;LICENZA&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;.Lucida Grande UI&apos;;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;Andama è un software gratuito: puoi redistribuirlo e/o modificarlo in base ai termini di licenza GNU General Public License 3 come pubblicato dalla Free Software Foundation. &lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;Andama è distribuito nella speranza che vi sarà utile, ma senza alcuna garanzia; senza nemmeno la garanzia implicita di COMMERCIABILITÀ o IDONEITÀ PER UN PARTICOLARE SCOPO.  Vedi la GNU General Public License per maggiori dettagli.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;.Lucida Grande UI&apos;; color:#ffffff;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="About.ui" line="147"/>
+        <source>&lt;a href=&quot;http://www.andama.org&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Community&lt;/span&gt;&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;http://www.andama.org&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Comunità&lt;/span&gt;&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="About.ui" line="165"/>
+        <source>&lt;a href=&quot;https://github.com/YiannisBourkelis/Andama-Remote-Desktop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Source code&lt;/span&gt; &lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://github.com/YiannisBourkelis/Andama-Remote-Desktop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Codice sorgente&lt;/span&gt; &lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="About.cpp" line="46"/>
+        <source>Version %1</source>
+        <translation>Versione %1</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <location filename="mainwindow.ui" line="683"/>
+        <source>Andama</source>
+        <translation>Andama</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="108"/>
+        <source>Local Computer</source>
+        <translation>Computer Locale</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <source>Remote Computer</source>
+        <translation>Computer Remoto</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <source>Your ID:</source>
+        <translation>Il tuo ID:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="177"/>
+        <location filename="mainwindow.ui" line="205"/>
+        <source>Password:</source>
+        <translation>Password:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="233"/>
+        <source>Remote ID:</source>
+        <translation>ID remoto:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="262"/>
+        <location filename="mainwindow.ui" line="364"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="335"/>
+        <source>Connect to computer</source>
+        <translation>Connetti</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="424"/>
+        <source>With the following ID and Password you can remotely connect to this computer.</source>
+        <translation>Potrai collegarti da remoto a questo computer utilizzando il seguente ID e Password.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="455"/>
+        <source>Enter the ID and Password of the remote computer you want to connect.</source>
+        <translation>Inserisci ID e Password del computer remoto alla quale ti vuoi collegare.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="561"/>
+        <location filename="mainwindow.ui" line="668"/>
+        <source>TextLabel</source>
+        <translation>Etichetta di testo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="622"/>
+        <source>Connecting...</source>
+        <translation>Connessione in corso...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="691"/>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="696"/>
+        <source>d-1</source>
+        <translation>d-1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="701"/>
+        <source>d-2</source>
+        <translation>d-2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="706"/>
+        <source>d-3</source>
+        <translation>d-3</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="402"/>
+        <source>P2P port: %1</source>
+        <translation>P2P Porta: %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="405"/>
+        <source>P2P internal port: %1, external port: %2</source>
+        <translation>P2P Porta interna: %1, porta esterna: %2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="408"/>
+        <source>UPnP Successful. %1 (%2)</source>
+        <translation>UPnP Successful. %1 (%2)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="413"/>
+        <source>UPnP returned error code: %1 (%2)</source>
+        <translation>Codice errore UPnP: %1 (%2)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="548"/>
+        <source>Disconnected from remote computer. Ready!</source>
+        <translation>Disconnesso dal computer remoto. Pronto!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="552"/>
+        <source>P2P Connection: Disconnected from remote computer</source>
+        <translation>Connessione P2P: Disconnesso dal computer remoto</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="563"/>
+        <location filename="mainwindow.cpp" line="757"/>
+        <source>Remote computer disconnected. Ready!</source>
+        <translation>Computer remoto disconnesso. Pronto!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="573"/>
+        <source>Another user is already connected to this computer.&lt;br /&gt;Are you sure that you want to exit?</source>
+        <translation>Un altro utente è già connesso a questo computer.&lt;br/&gt;Sei sicuro di voler uscire?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="627"/>
+        <location filename="mainwindow.cpp" line="658"/>
+        <location filename="mainwindow.cpp" line="667"/>
+        <location filename="mainwindow.cpp" line="787"/>
+        <source>Ready!</source>
+        <translation>Pronto!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="630"/>
+        <source>Application ID recieved from proxy. Ready to connect and accept connections!</source>
+        <translation>ID applicazione ricevuto dal proxy. Pronto a connettermi e ricevere connessioni!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="643"/>
+        <source>Remote computer accepted the connection. Requesting remote desktop image</source>
+        <translation>Il computer remoto ha accettato la connessione. Richiedo il desktop remoto</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="654"/>
+        <source>Remote ID does not exist</source>
+        <translation>L&apos;ID remoto non esiste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="659"/>
+        <source>The requested remote computer ID does not exist</source>
+        <translation>L&apos;ID del computer remoto richiesto non esiste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="663"/>
+        <location filename="mainwindow.cpp" line="683"/>
+        <source>The password is not correct</source>
+        <translation>La password non è corretta</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="668"/>
+        <location filename="mainwindow.cpp" line="687"/>
+        <source>The password you entered is not correct</source>
+        <translation>La password che hai inserito non è corretta</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="674"/>
+        <source>IP banned</source>
+        <translation>IP bannato</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="677"/>
+        <source>The connection is rejected because of multiple wrong password attempts</source>
+        <translation>La connessione è stata rifiutata a cause dell&apos;inserimento errato della password per più volte</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="690"/>
+        <source>Remaining tries: %1</source>
+        <translation>Tentativi rimasti: %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="699"/>
+        <source>The current version of the application is no longer supported.</source>
+        <translation>La versione corrente dell&apos;applicazione non è più supportata.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="700"/>
+        <source>Pease upgrade</source>
+        <translation>Per favore aggiorna</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="703"/>
+        <source>Upgrade required</source>
+        <translation>Aggiornamento richiesto</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="709"/>
+        <source>Do you want to download the newer version?</source>
+        <translation>Vuoi scaricare la nuova versione?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="727"/>
+        <source>Remote client accepted</source>
+        <translation>Il client ha accettato la connessione remota</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="738"/>
+        <source>Error connecting. %1</source>
+        <translation>Errore di connessione. %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="738"/>
+        <location filename="mainwindow.cpp" line="746"/>
+        <source>Please make sure you have an internet connection and try again.</source>
+        <translation>Per favore assicurati che la connessione ad internet funzioni e riprova.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="746"/>
+        <source>Error connecting with proxy server. %1</source>
+        <translation>Errore di connessione al proxy server. %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="758"/>
+        <source>The remote computer is disconnected</source>
+        <translation>Il computer remoto si è disconnesso</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="764"/>
+        <source>Warning! Your IP will be banned because of multiple wrong ID attempts. Remaining tries: %1</source>
+        <translation>Attenzione! Il tuo IP verrà bannato a causa dell&apos;inserimento di ID sbagliati per più volte. Tentativi rimanenti: %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="776"/>
+        <source>Connection rejected because of multiple wrong ID attempts. Please try again after %1 sec.</source>
+        <translation>Connessione rifiutata a causa dell&apos;inserimento di ID sbagliati per più volte. Per favore riprova fra %1 secondi.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="789"/>
+        <source>Cannot connect to the same ID</source>
+        <translation>Non puoi collegarti allo stesso ID</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="790"/>
+        <source>Hey! I cannot connect to myself</source>
+        <translation>Hey! Non posso collegarmi a me stesso</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="968"/>
+        <source>Connecting to remote computer...</source>
+        <translation>Connessione al computer remoto...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="986"/>
+        <source>Connecting to the remote computer directly...</source>
+        <translation>Connessione diretta al computer remoto...</translation>
+    </message>
+</context>
+</TS>
